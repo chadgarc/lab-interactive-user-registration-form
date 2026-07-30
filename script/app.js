6633ee0a2dc366b8626649bc0bded6cb37099ad2
Christian Garlen <chadgarval@outlook.com>
2026-07-30 00:57:55 -0400

    added username and email validation before submit, password validation, functions to support validations and keep clean code

--- script/app.js
+++ script/app.js
@@ -1,9 +1,13 @@
 
 // User names
 
-const usernames = new Array();
+const forms = new Array();
 
 const REGEXPWDVALIDATION = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+const USERINVALIDMESSAGE = "Enter a valid username with at least 6 characters";
+const EMAILINVALIDMESSAGE = "Enter a valid email";
+const PASSWORDINVALIDMESSAGE = "Enter a valid password";
+const CONFPASSWORDINVALIDMESSAGE = "Password doesn't match";
 const REGEXEMAILVALIDATION = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 const REGEXUSERNAME = /^[A-Za-z0-9_]{6,}$/;
 
@@ -11,28 +15,180 @@ const usernameInput = document.getElementById("username");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const confPasswordInput = document.getElementById("confirmPassword");
+const registrationForm = document.getElementById("registrationForm"); 
 
-function verifyPassword(input){
-    const span = confPasswordInput.nextElementSibling;
+// Global flag
+let userExist = false;
+let emailExist = false;
+
+// Verify if both passwords match
+verifyPassword = (input) => {
     if(confPasswordInput.value !== passwordInput.value){
         fieldColor(confPasswordInput,"red");
-        span.textContent = "Password doesn't match";
-        console.log("Password doesn't match");
+        changeSpan(confPasswordInput,CONFPASSWORDINVALIDMESSAGE);
+
     } else if( confPasswordInput.value === "" ){
         fieldColor(confPasswordInput,"red");
-        span.textContent = "Cannot be empty";
+        changeSpan(confPasswordInput,"Cannot be empty");
         fieldColor(confPasswordInput,"red");
+        
     } else {
         fieldColor(confPasswordInput,"green");
-        span.textContent = "";
-        console.log("Password match");
+        changeSpan(confPasswordInput,"");
     }
 }
 
+// change text inside form easily
+changeSpan = (element,text) => {
+    const span = element.nextElementSibling.nextElementSibling;
+    span.textContent = text;
+}
+
+// Check automatically the status of each element before submit
+fieldStatus = (element, type, exist = false) => {
+    let text = "";
+    switch (type){
+        case "username":
+            text = USERINVALIDMESSAGE;
+            break;
+        case "email":
+            text = EMAILINVALIDMESSAGE;
+            break;
+        case "password":
+            text = PASSWORDINVALIDMESSAGE;
+            break;
+        case "confirmPassword":
+            text = CONFPASSWORDINVALIDMESSAGE;
+            break;
+    } 
+    if(!element.validity.valid){
+        changeSpan(element, text);
+        return true;
+    } 
+
+    if(type === "username" && exist){
+        changeSpan(element, "Username already exist");
+        return true;
+    }
+
+    if(type === "email" && exist){
+        changeSpan(element, "Email already enrolled");
+        return true;
+    }
+
+    changeSpan(element,"");
+    return false;
+}
+
+// This validates if all entries are already filled before submit
+// If it will change the text if some fields are already filled
+verifyValidation = (event) => {
+    let invalid = false;
+
+    // Return false if meet requirements, true if not
+    if( fieldStatus(usernameInput, "username", userExist) ||
+        fieldStatus(emailInput, "email", emailExist) ||
+        fieldStatus(passwordInput, "password") ||
+        fieldStatus(confPasswordInput, "confirmPassword") ){
+            invalid = true;
+    }
+
+    if(invalid){
+        return;
+
+    } else {
+        // if valid it will add a new form data to our array
+        const newForm = new FormData(registrationForm);
+
+        // Delete unnecesary data from our form
+        newForm.delete("confirmPassword");
+
+        // Add form data into our array
+        forms.push(newForm);
+
+        // Reset all entries values
+        registrationForm.reset();
+
+        // Reset entries statuses 
+        document.querySelectorAll("input").forEach(input => {
+            input.classList.remove("touched");
+        });
+    }
+}
+
+// This listener will add touched class, so fields will turn red
+// if invalid
+document.querySelectorAll("input").forEach(input => {
+    input.addEventListener("blur", () => {
+        input.classList.add("touched");
+    })
+});
+
+// Listener to verify if password match
 confPasswordInput.addEventListener("input", input => verifyPassword(input));
 
+// Listener to match if password match, only evaluates confirm password
 passwordInput.addEventListener("input", input => verifyPassword(input));
 
+// Test method to verify if username meets criteria
+usernameInput.addEventListener("input", input => {
+    userExist = false;
+    forms.forEach(form => {
+        if(form.get("username").toLowerCase() === input.target.value.toLowerCase()){
+            userExist = true;
+        }
+    });
+    if(userExist){
+        changeSpan(usernameInput, "Username already exist");
+        fieldColor(usernameInput,"red");
+    } else {
+        changeSpan(usernameInput, "");
+        if(!REGEXUSERNAME.test(usernameInput.value)){
+            changeSpan(usernameInput, USERINVALIDMESSAGE);
+            fieldColor(usernameInput,"red");
+        } else {
+            changeSpan(usernameInput, "");
+            fieldColor(usernameInput,"green");
+        }
+    }
+});
+
+// Test method to verify if email meets criteria
+emailInput.addEventListener("input", input => {
+    emailExist = false;
+
+    forms.forEach(form => {
+        if(form.get("email").toLowerCase() === input.target.value.toLowerCase()){
+            emailExist = true;
+        }
+    });
+
+    if(emailExist){
+        changeSpan(emailInput, "Email already enrolled");
+        fieldColor(emailInput,"red");
+    } else {
+        if(!REGEXEMAILVALIDATION.test(emailInput.value)){
+            changeSpan(emailInput, EMAILINVALIDMESSAGE);
+            fieldColor(emailInput,"red");
+        } else {
+            changeSpan(emailInput, "");
+            fieldColor(emailInput,"green");
+        }
+    }
+});
+
+// Test method to verify if password meets criteria
+passwordInput.addEventListener("input", input => {
+    if(!REGEXPWDVALIDATION.test(passwordInput.value)){
+        changeSpan(passwordInput, PASSWORDINVALIDMESSAGE);
+        fieldColor(passwordInput,"red");
+    } else {
+        changeSpan(passwordInput, "");
+        fieldColor(passwordInput,"green");
+    }
+});
+
+// change highlight color around fields
 function fieldColor(field,color = "neutral"){
     const GREEN_BORDER = "input-success";
     const RED_BORDER = "input-error";
@@ -46,3 +202,10 @@ function fieldColor(field,color = "neutral"){
         field.classList.remove(GREEN_BORDER,RED_BORDER,".invalidField",".validField");
     }
 }
+
+// Preventing from refresh page after click submit button
+// Calls verify validation function
+registrationForm.addEventListener("submit", event => {
+    event.preventDefault(); // Avoid default form submision
+    verifyValidation(event);
+});
